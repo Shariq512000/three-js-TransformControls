992249702e19b3880dffe6976598af9a8da02829
Muhammad Shariq <muhammad.shariq@Ingendigital.net>
2023-07-14 23:45:22 -0700

    Buffer Geometry Basic

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0.5,0.5,0.5);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1 , 1000);
 camera.position.z = 30 ;
 
@@ -18,13 +19,46 @@ scene.add(ambient);
 
 // const texture = new THREE.TextureLoader().load("texture.jpg");
 // const bumpTexture = new THREE.TextureLoader().load("texture3.jpg");
-const geometry = new THREE.SphereGeometry(5 , 32, 32);
+const geometry = new THREE.BufferGeometry()
+const vertices = new Float32Array(
+    [
+        -1.0, -1.0, 1.0,
+        1.0, -1.0, 1.0,
+        1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+    ]
+);
 
-const material = new THREE.PointsMaterial({
-    color: "red",
-    size: 0.05,
-});
-const cube = new THREE.Points( geometry , material );
+const indeces = [
+    0, 1, 2,
+    2, 3, 0
+]
+
+const color = new Float32Array(
+    [
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        1.0, 1.0, 1.0
+    ]
+);
+
+const normal = new Float32Array(
+    [
+        1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0
+    ]
+);
+
+geometry.setAttribute('position' , new THREE.BufferAttribute(vertices , 3));
+geometry.setAttribute('color' , new THREE.BufferAttribute(color , 3));
+geometry.setAttribute('normal' , new THREE.BufferAttribute(normal , 3));
+geometry.setIndex(indeces);
+
+const material = new THREE.MeshStandardMaterial({vertexColors:true});
+const cube = new THREE.Mesh( geometry , material );
 
 // const cube = new THREE.Line( geometry , material ); if you use Line you don't need wireframe
 
