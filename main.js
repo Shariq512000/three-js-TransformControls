edaaab9e351b00c96d4c8f71a04552014440799a
Muhammad Shariq <muhammad.shariq@Ingendigital.net>
2023-07-15 00:16:55 -0700

    FirstPersonControls and FlyControls

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import * as THREE from "three";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {FirstPersonControls} from "three/examples/jsm/controls/FirstPersonControls";
+import {FlyControls} from "three/examples/jsm/controls/FlyControls";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0.5,0.5,0.5);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1 , 1000);
 camera.position.z = 30 ;
+camera.position.y = 10 ;
 
 
 const renderer = new THREE.WebGLRenderer();
@@ -16,48 +18,10 @@ const ambient = new THREE.AmbientLight(0xffffff , 1);
 scene.add(ambient);
 
 
+const geometry = new THREE.BoxGeometry(5, 5, 5);
 
-// const texture = new THREE.TextureLoader().load("texture.jpg");
-// const bumpTexture = new THREE.TextureLoader().load("texture3.jpg");
-const geometry = new THREE.BufferGeometry()
-const vertices = new Float32Array(
-    [
-        -1.0, -1.0, 1.0,
-        1.0, -1.0, 1.0,
-        1.0, 1.0, 1.0,
-        -1.0, 1.0, 1.0,
-    ]
-);
 
-const indeces = [
-    0, 1, 2,
-    2, 3, 0
-]
-
-const color = new Float32Array(
-    [
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0,
-        1.0, 1.0, 1.0
-    ]
-);
-
-const normal = new Float32Array(
-    [
-        1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0
-    ]
-);
-
-geometry.setAttribute('position' , new THREE.BufferAttribute(vertices , 3));
-geometry.setAttribute('color' , new THREE.BufferAttribute(color , 3));
-geometry.setAttribute('normal' , new THREE.BufferAttribute(normal , 3));
-geometry.setIndex(indeces);
-
-const material = new THREE.MeshStandardMaterial({vertexColors:true});
+const material = new THREE.MeshStandardMaterial({color:"red"});
 const cube = new THREE.Mesh( geometry , material );
 
 // const cube = new THREE.Line( geometry , material ); if you use Line you don't need wireframe
@@ -65,16 +29,26 @@ const cube = new THREE.Mesh( geometry , material );
 
 scene.add(cube);
 
+const gridHelper = new THREE.GridHelper(100 , 10);
+scene.add(gridHelper);
+
+const clock = new THREE.Clock()
 
 
-const controls = new OrbitControls(camera , renderer.domElement);
+const controls = new FirstPersonControls(camera , renderer.domElement);
+controls.movementSpeed = 150;
+controls.lookSpeed = 0.1;
+
+// const controls = new FlyControls(camera , renderer.domElement);
+// controls.movementSpeed = 1000;
+// controls.rollSpeed = Math.PI / 24;
 
 
 animate();
 
 function animate (){
 
-    controls.update();
+    controls.update(clock.getDelta());
 
     // cube.rotation.x += 0.02;
     // cube.rotation.y += 0.02;
